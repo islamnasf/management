99de17e2f4ed4d0a345e29c54d679053a4a77bf0
unknown <islamnasf19@gmail.com>
2025-12-21 18:18:04 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard/index');
+    return route('login');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
